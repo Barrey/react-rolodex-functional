c9b1c608da8ae42342fe651d0d67eef7d4180a07
Bari <ajibari@gmail.com>
2023-04-02 06:50:20 +0700

    convert
    convert from class to function

--- src/components/search-box/index.jsx
+++ src/components/search-box/index.jsx
@@ -1,15 +1,17 @@
-import { Component } from "react";
-import './search-box.styles.css';
+import "./search-box.styles.css";
 
-class SearchBox extends Component {
-  render() {
-    return (
-      <div className="search-box">
-        <input className={this.props.className} type='search' placeholder={this.props.placeholder} 
-        onChange={this.props.onChangeHandler} />
-      </div>
-    );
-  }
-}
+const SearchBox = (props) => {
+  const { className, placeholder, onChangeHandler } = props;
+  return (
+    <div className="search-box">
+      <input
+        className={className}
+        type="search"
+        placeholder={placeholder}
+        onChange={onChangeHandler}
+      />
+    </div>
+  );
+};
 
 export default SearchBox;
