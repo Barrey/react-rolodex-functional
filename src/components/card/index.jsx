5bcc0c9114f610a0540ccc4d9f12f4659845ddae
Bari <ajibari@gmail.com>
2023-04-02 06:51:57 +0700

    chore
    template string literal

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -6,7 +6,7 @@ const Card = (props) => {
     <div key={id} className="card-container">
       <img
         src={`https://robohash.org/${id}?set=set4&size=180x180`}
-        alt="`monster ${name}`"
+        alt={`monster ${name}`}
       />
       <h2>{name}</h2>
       <p>{email}</p>
